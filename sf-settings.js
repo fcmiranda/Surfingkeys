5651480e0a66dc1ce8eff3c323bde7c4d18f8336
fcmiranda <felip.3@hotmail.com>
2024-10-30 18:01:10 -0300

    update settings

--- sf-settings.js
+++ sf-settings.js
@@ -5,22 +5,12 @@ api.map('<Ctrl-l>', 'D'); //go foward in history
 api.cmap('<Ctrl-j>', '<Tab>'); //previous result item
 api.cmap('<Ctrl-k>', '<Shift-Tab>'); //next result item
 
-
-
 let currentScrollIndex = 0;
-
-65 const selectors = ['[data-tid="message-pane-list-viewport"]', '.virtual-tree-list-scroll-container']
-
-66 api.mapkey("cs", '#lcustom', function() {
-
-67 currentScrollIndex (currentScrollIndex + 1) % 2;
-
-68 const selector = document.querySelector(selectors (currentScrollIndex]);
-
-69 console.log(selector);
-
-70
-
-api.Hints.dispatchMouseClick(selector); api.Normal.highlightElement(selector, 200);
-
-71 72}, {domain: /teams.microsoft.com/i});
+const selectors = ['[data-tid="message-pane-list-viewport"]', '.virtual-tree-list-scroll-container']
+api.mapkey("cs", '#lcustom', function() {
+    currentScrollIndex = (currentScrollIndex + 1) % 2;
+    const selector = document.querySelector(selectors[currentScrollIndex]);
+    console.log(selector);
+    api.Hints.dispatchMouseClick(selector); 
+    api.highlightElement(selector, 200);
+}, {domain: /teams.microsoft.com/i});
